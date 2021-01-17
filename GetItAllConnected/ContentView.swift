effb15544c7eeff3b804e0b96e78549ceae4baea
Ed Cafferatra <cafferata@live.nl>
2021-01-17 11:10:22 +0100

    eerste opzet, settings goed zetten voor project.

---
+++ GetItAllConnected/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GetItAllConnected
+//
+//  Created by Ed Cafferata on 17/01/2021.(start)
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GetItAllConnected/GetItAllConnectedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetItAllConnectedApp.swift
+//  GetItAllConnected
+//
+//  Created by Ed Cafferata on 17/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct GetItAllConnectedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
